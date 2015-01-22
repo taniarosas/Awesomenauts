d1952a406a3b00a3d6bc7abc7a51c0b703c65c1b
tania rosas-jimenez <rosastania@yahoo.com>
2015-01-22 10:57:09 -0800

    commented for video 6

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -1,17 +1,27 @@
 // TODO
 game.PlayerEntity = me.Entity.extend({
+	//initializes the function 
 	init: function(x, y, settings){
 		this._super(me.Entity, 'init', [x,y, {
+			//makes the image of the player appear
 			image: "player",
+			//the width of the space created
 			width: 64,
+			//the height of the space created
 			height: 64,
+			//height for the player
 			spriteheight: "64",
+			//width for the player
 			spritewidth: "64",
+			//function of the shape of the screen 
 			getShape: function(){
+				//creates a rectangle of 64 by 64 so the player can go inside
+				//0 , 0 is the origin
 				return(new me.Rect(0, 0, 64, 64)).toPolygon();
 			}
 		}]);
 	},
+	//updates the function
 	update: function(){
 
 	}
--- js/screens/play.js
+++ js/screens/play.js
@@ -7,9 +7,9 @@ game.PlayScreen = me.ScreenObject.extend({
 		game.data.score = 0;
 		//telling what to look at as far as maps
 		me.levelDirector.loadLevel("level01");
-
+		//set a value to the variable player
 		var player = me.pool.pull("player", 0, 420, {});
-
+		//its sets the hieght of where they player appears  on the screen
 		me.game.world.addChild(player, 5);
 
 		// add our HUD to the game world
