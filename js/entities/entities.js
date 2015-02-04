dd9cf59644046e55aa42b5ca9c3785621e4876f2
tania rosas-jimenez <rosastania@yahoo.com>
2015-02-03 18:05:11 -0800

    coded for video 15

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -24,6 +24,8 @@ game.PlayerEntity = me.Entity.extend({
 		//y location changes
 		//it moved down to the platform
 		this.body.setVelocity(5, 20);
+
+		this.facing = "right";
 		//
 		me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
 		//adds animation to standing starting position
@@ -42,11 +44,14 @@ game.PlayerEntity = me.Entity.extend({
 			//sets the position of my x by adding the velocity defined above in setVelocity and multiplying it by me.timer.tick
 			//me.timer.tick makes the movement look smooth 
 			this.body.vel.x += this.body.accel.x * me.timer.tick;
+
+			this.facing = "right";
 			//makes the charcter face to right 
 			this.flipX(true);
 
 		}
 		else if(me.input.isKeyPressed("left")){
+				this.facing = "left";
 				this.body.vel.x -= this.body.accel.x * me.timer.tick;
 				this.flipX(false);
 		}
@@ -55,8 +60,8 @@ game.PlayerEntity = me.Entity.extend({
 			this.body.vel.x = 0;
 		}
 
-		if(me.input.isKeyPressed("jump") && !this.jumping && !this.falling){
-			this.jumping = true;
+		if(me.input.isKeyPressed("jump") && !this.body.jumping && !this.body.falling){
+			this.body.jumping = true;
 			this.body.vel.y -= this.body.accel.y * me.timer.tick;
 		}
 
@@ -69,6 +74,7 @@ game.PlayerEntity = me.Entity.extend({
 			}
 		}
 
+
 		//if the button is pushed then it will walk but it not it will execute the else code
 		else if(this.body.vel.x !==0 ){
 		if(!this.renderable.isCurrentAnimation("walk")){
@@ -79,13 +85,42 @@ game.PlayerEntity = me.Entity.extend({
 		this.renderable.setCurrentAnimation("idle");
 	}
 
+	/*if(me.input.isKeyPressed("attack")){
+			if(!this.renderable.isCurrentAnimation("attack")){
+				console.log(!this.renderable.isCurrentAnimation("attack"));
+				this.renderable.setCurrentAnimation("attack", "idle");
+				this.renderable.setAnimationFrame();
+			}
+		}*/
+
+		me.collision.check(this, true, this.collideHandler.bind(this), true);
+
 		//updates the function to true
 		this.body.update(delta);
 		//updates the animation
 		this._super(me.Entity, "update", [delta]);
 		return true;
 
+	},
+
+	collideHandler: function(response){
+		if(response.b.type==='EnemyBaseEntity'){
+			var ydif = this.pos.y - response.b.pos.y;
+			var xdif = this.pos.x - response.b.pos.x;
+
+			console.log("xdif " + xdif + " ydif " + ydif);
+			
+			if(xdif>-35 && this.facing==='right' && (xdif<0)){
+				this.body.vel.x = 0;
+				this.pos.x = this.pos.x -1;
+			}
+			else if(xdif<70 && this.facing==='left' && xdif>0){
+				this.body.vel.x = 0;
+				this.pos.x = this.pos.x +1;
+			}
+		}
 	}
+
 });
 
 	game.PlayerBaseEntity = me.Entity.extend({
