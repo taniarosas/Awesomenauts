2719fd9c1051a9e282606508acddf1683ea13ba8
tania rosas-jimenez <rosastania@yahoo.com>
2015-02-03 20:49:24 -0800

    coded for video 18

--- js/resources.js
+++ js/resources.js
@@ -12,6 +12,7 @@ game.resources = [
 	 {name: "player", type:"image", src: "data/img/orcSpear.png"},
 	 //added the tower image to load on the map
 	 {name: "tower", type:"image", src: "data/img/tower_round.svg.png"},
+	 {name: "creep1", type:"image", src: "data/img/brainmonster.png"},
 	/* Atlases 
 	 * @example
 	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
