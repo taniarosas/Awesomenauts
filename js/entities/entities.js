303dbdfc81d0beb6c8964619c0fb3d4de36dfb4f
tania rosas-jimenez <rosastania@yahoo.com>
2015-02-09 17:02:17 -0800

    coded for video 20

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -293,8 +293,11 @@ game.EnemyCreep = me.Entity.extend({
 		this.renderable.addAnimation("walk", [3, 4, 5], 80);
 		this.renderable.setCurrentAnimation("walk");
 	},
-	update: function(){
-
+	update: function(delta){
+		this.body.vel.x -= this.body.accel.x * me.timer.tick;
+		this.body.update(delta);
+		this._super(me.Entity, "update", [delta]);
+		return true;
 	}
 });
 
