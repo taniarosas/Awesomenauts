9aa9e133a6e86d0be0509c4de1218b4c980171c7
tania rosas-jimenez <rosastania@yahoo.com>
2015-03-03 11:20:51 -0800

    commented on video 38

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -70,27 +70,32 @@ game.ExperienceManager = Object.extend({
 	update: function(){
 		//if you win the game is not over
 		if(game.data.win === true && !this.gameOver){
+			//sets the gameOver to true
 			this.gameOver(true);
 		}
+		//you gain no points 
 		else if(game.data.win === false && !this.gameOver){
-	
+			//makes the game be over
 			this.gameOver(false);
 		}
 		//makes the code run
 		return true;
 	},
+	//made a gameOver function
 	gameOver: function(win){
+		//if you win,
 		if(win){
 			//adds ten points to the experience variable
 			game.data.exp += 10;
 		}
+		//if else you gain 1 point
 		else{
 			//adds 1 point to the experience variable
 			game.data.exp += 1;
 		}
 		//makes the game be over
 		this.gameOver = true;
-
+		//saves the experience 
 		me.save.exp = game.data.exp;
 	}
 });
