82d786b1ef729a4e877a5e96333d94b4a38a19ab
tania rosas-jimenez <rosastania@yahoo.com>
2015-03-11 19:54:35 -0700

    coded for video 44

--- js/screens/spendExp.js
+++ js/screens/spendExp.js
@@ -7,6 +7,12 @@ game.SpendExp = me.ScreenObject.extend({
 		//call our image
 		//-10 is our z layer, it puts the title screen way in the back
 		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('exp-screen')), -10); // TODO
+
+		me.input.bindKey(me.input.KEY.F1, "F1");
+		me.input.bindKey(me.input.KEY.F2, "F2");
+		me.input.bindKey(me.input.KEY.F3, "F3");
+		me.input.bindKey(me.input.KEY.F4, "F4");
+		me.input.bindKey(me.input.KEY.F5, "F5");
 		
 		//add some text
 		me.game.world.addChild(new (me.Renderable.extend({
@@ -17,7 +23,7 @@ game.SpendExp = me.ScreenObject.extend({
 				//changed x, y , width and height
 				this._super(me.Renderable, 'init', [10, 10, 300, 50]);
 				//initialization of text
-				this.font = new me.Font("Arial", 46, "white");
+				this.font = new me.Font("Arial", 26, "white");
 			},
 			//draw whats on the screen 
 			//passing whatever our renderer is as its parameter
@@ -26,18 +32,40 @@ game.SpendExp = me.ScreenObject.extend({
 				//changed the position of start new game
 				this.font.draw(renderer.getContext(), "PRESS F1-F4 TO BUY, F5 TO SKIP", this.pos.x, this.pos.y);
 				this.font.draw(renderer.getContext(), "CURRENT EXP: " + game.data.exp.toString(), this.pos.x + 100, this.pos.y + 50);
-				this.font.draw(renderer.getContext(), "F1: INCREASE GOLD PRODUCTION" + game.data.exp.toString(), this.pos.x + 200, this.pos.y + 100);
-				this.font.draw(renderer.getContext(), "F2: ADD STARTING GOLD" + game.data.exp.toString(), this.pos.x + 200, this.pos.y + 150);
-				this.font.draw(renderer.getContext(), "F3: INCREASE ATTACK DAMAGE" + game.data.exp.toString(), this.pos.x + 300, this.pos.y + 200);
-				this.font.draw(renderer.getContext(), "F4: INCREASE STARTING HEALTH" + game.data.exp.toString(), this.pos.x + 400, this.pos.y + 250);
+				this.font.draw(renderer.getContext(), "F1: INCREASE GOLD PRODUCTION! CURRENT LEVEL: " + game.data.exp1.toString() + " COST: " + ((game.data.exp1 + 1) * 10), this.pos.x, this.pos.y + 100);
+				this.font.draw(renderer.getContext(), "F2: ADD STARTING GOLD", this.pos.x, this.pos.y + 150);
+				this.font.draw(renderer.getContext(), "F3: INCREASE ATTACK DAMAGE", this.pos.x, this.pos.y + 200);
+				this.font.draw(renderer.getContext(), "F4: INCREASE STARTING HEALTH", this.pos.x, this.pos.y + 250);
 			}
 		})));
-},
+		this.handler = me.event.subscribe(me.event.KEYDOWN, function(action, keyCode, edge) {
+			if (action === "F1") {
+
+			}
+			else if (action === "F2") {
+
+			}
+			else if (action === "F3") {
+
+			}
+			else if (action === "F4") {
+
+			}
+			else if (action === "F5") {
+				me.state.change(me.state.PLAY);
+			}
+		});
+	},
 	
 	/**	
 	 *  action to perform when leaving this screen (state change)
 	 */
 	onDestroyEvent: function() {
-	
+		me.input.unbindKey(me.input.KEY.F1, "F1");
+		me.input.unbindKey(me.input.KEY.F2, "F2");
+		me.input.unbindKey(me.input.KEY.F3, "F3");
+		me.input.unbindKey(me.input.KEY.F4, "F4");
+		me.input.unbindKey(me.input.KEY.F5, "F5");
+		me.event.unsubscribe(this.handler);
 	}
 });
