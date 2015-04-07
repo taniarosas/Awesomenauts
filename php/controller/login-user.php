a10336ff5d537132282a0c6eea47d59b5da10efc
tania rosas-jimenez <rosastania@yahoo.com>
2015-04-06 21:36:29 -0700

    coded for video 57

--- php/controller/login-user.php
+++ php/controller/login-user.php
@@ -33,8 +33,8 @@ if ($query->num_rows == 1) {
 		$_SESSION["name"] = $username;
         echo json_encode($array);
     } else {
-        echo "<p>Invalid username and password</p>";
+        echo "Invalid username and password";
     }
 } else {
-    echo "<p>Invalid username and password</p>";
+    echo "Invalid username and password";
 }
