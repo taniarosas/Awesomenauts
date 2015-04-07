d01b4a45841e8bdb051030130f46ffeb6c521635
tania rosas-jimenez <rosastania@yahoo.com>
2015-04-07 11:07:25 -0700

    coded for video 64

--- js/entities/MiniMap.js
+++ js/entities/MiniMap.js
@@ -1,3 +1,17 @@
-game.EnemyBaseEntity = me.Entity.extend({
+game.MiniMap = me.Entity.extend({
+	init: function(x, y, settings) {
+		this._super(me.Entity, "init", [x, y, {
+			image: "minimap", 
+			width: 279,
+			height: 155,
+			spritewidth: "279",
+			spriteheight: "155",
+			getShape: function() {
+				return (new me.Rect(0, 0, 279, 155)).toPolygon();
+			}
+		}]);
+		this.floating = true;
+	}
+ })
 	
 });
