725dee676d702ba6786b7a581c5912daf4b428a1
tania rosas-jimenez <rosastania@yahoo.com>
2015-04-06 21:55:37 -0700

    coded for video 58

--- js/screens/spendExp.js
+++ js/screens/spendExp.js
@@ -13,7 +13,7 @@ game.SpendExp = me.ScreenObject.extend({
 		me.input.bindKey(me.input.KEY.F3, "F3");
 		me.input.bindKey(me.input.KEY.F4, "F4");
 		me.input.bindKey(me.input.KEY.F5, "F5");
-		var exp1cost = ((game.data.exp1 + 1) * 10);
+		var exp1cost = ((Number(game.data.exp1) + 1) * 10);
 		
 		//add some text
 		me.game.world.addChild(new (me.Renderable.extend({
